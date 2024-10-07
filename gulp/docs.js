8afd9c63504fd1bbdcb48bd81d532339f4a7003b
sxtim <sxtim@gmail.com>
2024-10-08 00:13:06 +0300

    disable tag picture

--- gulp/docs.js
+++ gulp/docs.js
@@ -58,7 +58,7 @@ gulp.task('html:docs', function () {
 		.pipe(changed('./docs/'))
 		.pipe(plumber(plumberNotify('HTML')))
 		.pipe(fileInclude(fileIncludeSetting))
-		.pipe(webpHTML())
+		// .pipe(webpHTML())
 		.pipe(htmlclean())
 		.pipe(gulp.dest('./docs/'));
 });
