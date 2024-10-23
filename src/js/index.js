46bb02833f93591e48f3c0b29287a5b7d651d178
sxtim <sxtim@gmail.com>
2024-10-24 00:13:06 +0300

    refactor slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,39 @@
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    parallax: true,
+    speed: 1000,
+
+    keyboard: {
+        enabled: true,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.slider-controls__count',
+        type: 'fraction',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '#sliderNext',
+        prevEl: '#sliderPrev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
