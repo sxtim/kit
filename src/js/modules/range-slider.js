d5940120ee0f263bba0c862966b2c04d30a87bd5
sxtim <sxtimx@gmail.com>
2024-11-02 14:37:06 +0300

    added square slider to filter

--- src/js/modules/range-slider.js
+++ src/js/modules/range-slider.js
@@ -1,25 +1,26 @@
 import noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 
-const rangeSlider = document.getElementById('range-slider');
+const priceSlider = document.getElementById('price-slider');
 
-if (rangeSlider) {
-  noUiSlider.create(rangeSlider, {
-    start: [40, 120],
+if (priceSlider) {
+  noUiSlider.create(priceSlider, {
+    start: [4000000, 120000000],
     connect: true,
-    step: 10,
+    step: 500,
     range: {
-      'min': [40],
-      'max': [120]
+      'min': [4000000],
+      'max': [120000000]
     }
   });
 
-  const input0 = document.getElementById('input-0');
-  const input1 = document.getElementById('input-1');
-  const inputs = [input0, input1];
+  const inputPriceMin = document.getElementById('input-price-min');
+  const inputPriceMax = document.getElementById('input-price-max');
+  const inputs = [inputPriceMin, inputPriceMax];
 
 
-  rangeSlider.noUiSlider.on('update', function(values, handle){
+  priceSlider.noUiSlider.on('update', function(values, handle){
+    console.log(values)
     inputs[handle].value = Math.round(values[handle]);
   });
 
@@ -29,7 +30,47 @@ if (rangeSlider) {
 
     console.log(arr);
 
-    rangeSlider.noUiSlider.set(arr);
+    priceSlider.noUiSlider.set(arr);
+  };
+
+  inputs.forEach((el, index) => {
+    el.addEventListener('change', (e) => {
+      console.log(index);
+      setRangeSlider(index, e.currentTarget.value);
+    });
+  });
+}
+
+const squareSlider = document.getElementById('square-slider');
+
+if (squareSlider) {
+  noUiSlider.create(squareSlider, {
+    start: [40, 120],
+    connect: true,
+    step: 10,
+    range: {
+      'min': [40],
+      'max': [120]
+    }
+  });
+
+  const inputSquareMin = document.getElementById('input-square-min');
+  const inputSquareMax = document.getElementById('input-square-max');
+  const inputs = [inputSquareMin, inputSquareMax];
+
+
+  squareSlider.noUiSlider.on('update', function(values, handle){
+    console.log(values)
+    inputs[handle].value = Math.round(values[handle]);
+  });
+
+  const setRangeSlider = (i, value) => {
+    let arr = [null, null];
+    arr[i] = value;
+
+    console.log(arr);
+
+    squareSlider.noUiSlider.set(arr);
   };
 
   inputs.forEach((el, index) => {
