22f39806c235269369e47beece5fd76d875d949c
sxtim <sxtim@gmail.com>
2024-12-30 13:35:37 +0300

    added apartment similar section

--- src/js/index.js
+++ src/js/index.js
@@ -64,39 +64,18 @@ const swiperTabsContent = new Swiper('.swiper-tabs-content', {
     },
 });
 
-//MODAL TABS APARTMENT
-
-// Открыть модальное окно
-document.getElementById("open-modal-btn1").addEventListener("click", function() {
-  document.getElementById("my-modal").classList.add("open")
-})
-
-
-// Закрыть модальное окно
-document.getElementById("close-my-modal-btn").addEventListener("click", function() {
-  document.getElementById("my-modal").classList.remove("open")
-})
-
-// Закрыть модальное окно при нажатии на Esc
-window.addEventListener('keydown', (e) => {
-  if (e.key === "Escape") {
-    document.getElementById("my-modal").classList.remove("open")
-  }
-});
-
-// Закрыть модальное окно при клике вне его
-document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
-  event._isClickWithInModal = true;
-});
-document.getElementById("my-modal").addEventListener('click', event => {
-  if (event._isClickWithInModal) return;
-  event.currentTarget.classList.remove('open');
+var swiperApartmentSimilar = new Swiper(".swiperApartmentSimilar", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".ap-sim-swiper-pagination",
+        clickable: true,
+    },
 });
 
 
 
 //FSLIGHTBOX
-
 refreshFsLightbox();
 
 
