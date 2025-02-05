4207fd1c6639c0bff9150f9f015cab44744f6afd
sxtim <sxtimx@gmail.com>
2025-02-05 17:39:33 +0300

    added uk page

--- src/js/index.js
+++ src/js/index.js
@@ -1,101 +1,97 @@
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js"
 
-mobileNav();
-import './modules/tabs';
-import './modules/range-slider';
+mobileNav()
+import "./modules/tabs"
+import "./modules/range-slider"
 
-require("fslightbox");
-
+require("fslightbox")
 
 //SLIDER MAIN
 // import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper/bundle"
 
 // import styles bundle
-import 'swiper/css/bundle';
+import "swiper/css/bundle"
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  parallax: true,
-  speed: 1000,
+const swiper = new Swiper(".swiper", {
+	// Optional parameters
+	direction: "horizontal",
+	loop: true,
+	parallax: true,
+	speed: 1000,
 
-  keyboard: {
-    enabled: true,
-  },
+	keyboard: {
+		enabled: true,
+	},
 
-  // If we need pagination
-  pagination: {
-    el: '.slider-controls__count',
-    type: 'fraction',
-  },
+	// If we need pagination
+	pagination: {
+		el: ".slider-controls__count",
+		type: "fraction",
+	},
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '#sliderNext',
-    prevEl: '#sliderPrev',
-  },
+	// Navigation arrows
+	navigation: {
+		nextEl: "#sliderNext",
+		prevEl: "#sliderPrev",
+	},
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
+	// And if we need scrollbar
+	scrollbar: {
+		el: ".swiper-scrollbar",
+	},
+})
 
 //SLIDER TABS APARTMENT
-const swiperTabsNav = new Swiper('.apartment-swiper-tabs-nav', {
-  spaceBetween: 20,
-  slidesPerView: 4,
-});
+const swiperTabsNav = new Swiper(".apartment-swiper-tabs-nav", {
+	spaceBetween: 20,
+	slidesPerView: 4,
+})
 
 // Swiper Content
-const swiperTabsContent = new Swiper('.apartment-swiper-tabs-content', {
-  spaceBetween: 0,
-  allowTouchMove: false,
-  loop: false,
-  autoHeight: true,
-  longSwipes: true,
-  resistanceRatio: 0, // Disable First and Last Swiper
-  watchOverflow: true,
-  loopedSlides: 5,
-  thumbs: {
-    swiper: swiperTabsNav,
-  },
-});
+const swiperTabsContent = new Swiper(".apartment-swiper-tabs-content", {
+	spaceBetween: 0,
+	allowTouchMove: false,
+	loop: false,
+	autoHeight: true,
+	longSwipes: true,
+	resistanceRatio: 0, // Disable First and Last Swiper
+	watchOverflow: true,
+	loopedSlides: 5,
+	thumbs: {
+		swiper: swiperTabsNav,
+	},
+})
 
 //SLIDER APARTMENT
 var swiperApartmentSimilar = new Swiper(".apartment-similar-swiper", {
-  slidesPerView: 1.2,
-  spaceBetween: 30,
-  pagination: {
-    el: ".apartment-similar-swiper-pag",
-    clickable: true,
-  },
-  breakpoints: {
-    360: {
-      slidesPerView: 1.4
-    },
-    768: {
-      slidesPerView: 2
-    },
-    998: {
-      slidesPerView: 3.2
-    }
-  },
-});
-
+	slidesPerView: 1.2,
+	spaceBetween: 30,
+	pagination: {
+		el: ".apartment-similar-swiper-pag",
+		clickable: true,
+	},
+	breakpoints: {
+		360: {
+			slidesPerView: 1.4,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		998: {
+			slidesPerView: 3.2,
+		},
+	},
+})
 
 //FSLIGHTBOX
-refreshFsLightbox();
+refreshFsLightbox()
 
 let sliderOne = new Swiper(".commerce-info__slider", {
-  pagination: {
-    el: ".commerce-info__slider-pagination",
-    clickable: true
-  },
-  loop: true
-});
-
+	pagination: {
+		el: ".commerce-info__slider-pagination",
+		clickable: true,
+	},
+	loop: true,
+})
